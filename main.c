dda65ed76a3886e3e712d2935401f83dd85b1ac6
Muntasir Wahed <immuntasir@gmail.com>
2017-02-07 19:55:17 +0600

    Update main.c

--- main.c
+++ main.c
@@ -5,7 +5,8 @@
 #include <string.h>
 void lifetimeLoop();
 void getArgsFromLine(int *argc, char *line, char args[100][100]);
-int execute (int argc, char args[100][100]);
+int execute (char ** arg, int in, int out, int background);
+int executeIt (int argc, char args[100][100]);
 void executeCD(char args[100]);
 void executeEcho();
 void executePwd();
@@ -24,8 +25,8 @@ void executeExit() {
     printf("Good bye, cruel world.\n");
     exit(EXIT_SUCCESS);
 }
-void executeEcho(int argc, char args[100][100]) {
-    for (int i=1; i<argc; i++) printf("%s ", args[i]);
+void executeEcho(char **arg) {
+    for (int i=1; arg[i] != NULL; i++) printf("%s ", arg[i]);
     printf("\n");
     printf("That was pretty easy. Try again, ayyo, lmao\n");
 }
@@ -35,43 +36,79 @@ void executeHelp() {
     printf("You have to figure it out yourself.\n");
     printf("Check www.google.com for more\n");
 }
-int execute (int argc, char args[100][100]) {
+int execute (char ** arg, int in, int out, int background) {
     pid_t pid, wpid;
     int status;
-    pid = fork();
-    char **arg[100];
-    if (pid == 0) {
-        // Child process
-        //system(args[0]);
-        int i;
-        memset(arg, 0, sizeof arg);
-        //for (i=0; i<10; i++) printf("%d\n", arg[i]);
-        for (i=0; i<argc; i++) {
-            arg[i] = args[i];
-            //printf("%d\n", arg[i]);
-        }
-        //for (i=0; i<10; i++) printf("%p %s\n", arg[i], arg[i]);
-
-        //printf("1");
-        //printf("%s\n", arg[1]);
-        //if (strlen(arg[1]) == 0)
-            //arg[1] = 0;
-        //arg[2] = 0;
-        if (execvp(arg[0], arg) == -1) {
-            perror("Error: ");
-        }
-        exit(EXIT_FAILURE);
-    }
-
-    else if (pid < 0) perror("Error occured while forking"); // well, it explains itself
+    if (strcmp(arg[0], "cd") == 0) executeCD(arg[1]);
+        else if (strcmp(arg[0], "echo") == 0) executeEcho(arg);
+        else if (strcmp(arg[0], "pwd") == 0) executePwd();
+        else if (strcmp(arg[0], "help") == 0) executeHelp();
+        else if (strcmp(arg[0], "exit") == 0) executeExit();
     else {
-    // Parent process
-    do {
-      wpid = waitpid(pid, &status, WUNTRACED);
-    } while (!WIFEXITED(status) && !WIFSIGNALED(status));
-  }
+        pid = fork();
 
-  return 1;
+        if (pid == 0) {
+            // Child process
+            if(in!=0)	{
+				dup2(in,0);
+				close(in);
+			}
+			if(out!=1)	{
+				dup2(out,1);
+				close(out);
+            }
+            if (execvp(arg[0], arg) == -1) {
+                perror("Error: ");
+            }
+            exit(EXIT_FAILURE);
+        }
+
+        else if (pid < 0) perror("Error occured while forking"); // well, it explains itself
+        else {
+        // Parent process
+        do {
+          wpid = waitpid(pid, &status, WUNTRACED);
+        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+      }
+    }
+    return 1;
+}
+int executeIt (int argc, char args[100][100]) {
+    char **arg[100];
+    int in, out, background;
+    int pipefd[2];
+    in = 0;
+    out = 1;
+    background = 0;
+    memset(arg, 0, sizeof arg);
+    int i = 0;
+    int cmdptr = 0;
+    if (args[argc-1][0] == '&') {
+        argc--;
+        background = 1;
+    }
+    for (i=0; i<argc; i++) {
+        if (args[i][0] == '|') {
+            arg[cmdptr] = NULL;
+            pipe(pipefd);
+            execute (arg, in, pipefd[1], background);
+            in = pipefd[0];
+            close(pipefd[1]);
+            cmdptr = 0;
+        }
+        else if (args[i][0] == '>') {
+
+        }
+        else if (args[i][0] == '<') {
+
+        }
+        else {
+            arg[i] = args[i];
+            cmdptr++;
+        }
+    }
+    arg[cmdptr] = 0;
+    execute(arg, in, out, background);
 }
 
 
@@ -96,20 +133,18 @@ void getArgsFromLine(int *argc, char *line, char args[100][100]) {
 void lifetimeLoop() {
     int isRunning = 1;
     int argc;
-    char line[100],  args[100][100];
-    memset(args, 0, sizeof 0);
+    char line[10000],  args[100][100];
+    memset(line, 0, sizeof line);
+    memset(args, 0, sizeof args);
     while (isRunning) {
         printf(">> ");
         gets(line);
         argc = 0;
         getArgsFromLine(&argc, line, args);
         int i;
-        if (strcmp(args[0], "cd") == 0) executeCD(args[1]);
-        else if (strcmp(args[0], "echo") == 0) executeEcho(argc, args);
-        else if (strcmp(args[0], "pwd") == 0) executePwd();
-        else if (strcmp(args[0], "help") == 0) executeHelp();
-        else if (strcmp(args[0], "exit") == 0) executeExit();
-        else isRunning = execute(argc, args);
+        isRunning = executeIt(argc, args);
+        memset(line, 0, sizeof line);
+        memset(args, 0, sizeof args);
     }
 
 }
